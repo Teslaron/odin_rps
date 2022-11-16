1d2cdbffa41d2cf6a1b56c52b5feccc815d96c80
Teslaron <117548948+Teslaron@users.noreply.github.com>
2022-11-16 14:32:55 +0100

    Version 0.1
    Created a Blank index Page with a script tag, and a JS File with
    the Pseudocode in it, with the logic for the game the work.

---
+++ javascript.js
@@ -0,0 +1,18 @@
+//Rock Paper Scissors
+    //Rock beats Scissor, Scissor beats Paper, Paper beats Rock
+//The Game is played through a the console
+    //The game is started through invoking a function through the console
+//User inputs Rock or Paper or Scissor
+    //Input will come through a Prompt
+        //Create an error message and retry if the User inputs a wrong word or number.
+    //Inputs need to be case insensitive
+//AI also randomly inputs Rock or Paper or Scissor
+    //Generate a number between 1-3 and pick AI input based on that number.
+//Program evaluates the 2 Inputs, declares a winner and outputs a notification
+    //Program also needs to declare a possible draw
+    //Notification should output both the Player and the AI choice and the winner of the round.
+    //The declared winner needs to be put out in a return statement so that 5 Rounds can be at the end.
+//The game is played for 5 Rounds, the one with the most wins is declared the ultimate winner
+    //The 5 Rounds will be played through a for loop
+    //The Program needs to keep track of the individual wins
+    //Program needs to output a notification with the Winner and the number of individual wins each.
