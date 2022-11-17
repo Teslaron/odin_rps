d6f4a125ab971c3c2ffcd4907142a2565db88d08
Teslaron <117548948+Teslaron@users.noreply.github.com>
2022-11-17 16:42:55 +0100

    Version 0.2
    Created a Player Input and a random AI input.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,22 @@
 //Rock Paper Scissors
     //Rock beats Scissor, Scissor beats Paper, Paper beats Rock
-//The Game is played through a the console
+//The Game is played through the console
     //The game is started through invoking a function through the console
 //User inputs Rock or Paper or Scissor
     //Input will come through a Prompt
-        //Create an error message and retry if the User inputs a wrong word or number.
     //Inputs need to be case insensitive
+function getPlayerChoice() {
+    let selection = prompt("Pick Rock, Paper, or Scissor"+"\n\n"+"If your pick is invalid the AI will pick Scissor.").toLowerCase();
+    selection==="rock" ?selection="Rock" :selection==="paper" ?selection="Paper" :selection="scissor"
+    return selection
+}
 //AI also randomly inputs Rock or Paper or Scissor
-    //Generate a number between 1-3 and pick AI input based on that number.
+    //Generate a number between 0-2 and pick AI input based on that number.
+function getComputerChoice() {
+    let number = Math.floor(Math.random()*3)
+    number == 0 ?number="Rock" :number == 1 ?number="Paper" :number="Scissor";
+    return number
+}
 //Program evaluates the 2 Inputs, declares a winner and outputs a notification
     //Program also needs to declare a possible draw
     //Notification should output both the Player and the AI choice and the winner of the round.
@@ -16,3 +25,4 @@
     //The 5 Rounds will be played through a for loop
     //The Program needs to keep track of the individual wins
     //Program needs to output a notification with the Winner and the number of individual wins each.
+
